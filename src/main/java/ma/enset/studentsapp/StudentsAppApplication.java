9b1aec939d5b0050e3ce4b1743df349052dedd90
Larbi SOUSSI <soussi123lrb@gmail.com>
2025-04-17 11:06:50 +0100

    ✅ Ajout de la configuration MySQL fonctionnelle

--- src/main/java/ma/enset/studentsapp/StudentsAppApplication.java
+++ src/main/java/ma/enset/studentsapp/StudentsAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class StudentsAppApplication implements CommandLineRunner {
     @Autowired
@@ -15,8 +17,37 @@ public class StudentsAppApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         productRepository.save(new Product(null,"Computer",4300,3));
         productRepository.save(new Product(null,"Printer",1200,4));
-        productRepository.save(new Product(null,"Computer",3200,32));
+        productRepository.save(new Product(null,"Smart Phone",3200,32));
+        List<Product> products = productRepository.findAll();
+        products.forEach(
+                p -> {
+                    System.out.println(p.toString());
+                }
+        );
 
+        Product product = productRepository.findById(Long.valueOf(1)).get();
+        System.out.println("***************");
+        System.out.println(product.getId());
+        System.out.println(product.getName());
+        System.out.println(product.getPrice());
+        System.out.println(product.getQuantity());
+        System.out.println("***************");
+        System.out.println("-----------------");
+        List<Product> productList = productRepository.findByNameContains("C");
+        productList.forEach(p->{
+            System.out.println(p);
+        });
+        System.out.println("-----------------");
+        List<Product> productList2 = productRepository.search("%C%");
+        productList.forEach(p->{
+            System.out.println(p);
+        });
+
+        System.out.println("-----------------");
+        List<Product> productList3 = productRepository.searchByPrice(3000);
+        productList3.forEach(p->{
+            System.out.println(p);
+        });
 
 
     }
